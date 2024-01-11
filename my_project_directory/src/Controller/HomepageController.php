dafad1f1e477f3d6a1e71e1eead8fc69c1502965
aswan1901 <aswan.josephmathieu1999@gmail.com>
2024-01-11 14:36:44 +0100

    exo-symfo

---
+++ my_project_directory/src/Controller/HomepageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomepageController extends AbstractController
+{
+    #[Route('/', name: 'app_homepage')]
+    public function index(ProductRepository $productRepository): Response
+    {
+        var_dump($productRepository->findAll());
+
+        return $this->render('base.html.twig', [
+            'controller_name' => 'HomepageController',
+            
+        ]);
+    }
+}
---
+++ my_project_directory/src/Controller/ProductController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Form\ProductType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    #[Route('/product', name: 'app_product')]
+        public function index(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $demo = $form->getData();
+            $entityManager->persist($demo);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_homepage');
+  }
+        return $this->render('product/index.html.twig', [
+            'formProduct' => $form
+        ]);
+  
+}
+}
